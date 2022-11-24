121bff6c3af73999481986575952bb30ecbed6a1
SombatBuekeaw <sombat073855@gmail.com>
2022-11-24 17:13:48 +0700

    first commit

---
+++ week601.php
@@ -0,0 +1,14 @@
+<?php
+$i = 1;
+for($i; $i<=12; $i = $i+1) {
+    print("2 x $i = ".(2*$i)."<br />");
+    $total += $i;
+}    
+print("$total = " .$total);
+
+
+
+
+
+
+?>
---
+++ week602.php
@@ -0,0 +1,8 @@
+<?php
+$seaso = array("Summer","Winter","Autumn");
+
+foreach($seaso as $arr){
+    print("Season is: $arr <br />");
+}
+
+?>
